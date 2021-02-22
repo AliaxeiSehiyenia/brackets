e2774b4994224589a3afee96d3c221547dac6bab
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-22 15:43:56 +0300

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracketOpen = bracketsConfig.map((elem) => elem[0]);
+  const bracketClose = bracketsConfig.map((elem) => elem[1]);
+  const arr = [];
+  const strIn = str.split("");
+
+  if (strIn.length % 2 !== 0) {
+    return false;
+  }
+  for (let i = 0; i <= strIn.length - 1; i++) {
+    if (bracketOpen.includes(strIn[i])) {
+      arr.push(strIn[i]);
+    }
+    
+    if (bracketClose.indexOf(strIn[i]) === bracketOpen.indexOf(arr[arr.length - 1])) {
+      arr.pop();
+    }
+
+    if (bracketOpen.includes(strIn[i]) && bracketClose.includes(strIn[i])) {
+      if (arr[arr.length - 1] === strIn[i]) {
+        arr.pop();
+      } else {
+        arr.push(strIn[i]);
+      }
+    }
+  }
+  return arr.length === 0;
+};
